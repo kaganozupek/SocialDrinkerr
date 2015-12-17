bf304bf99f01f0dbd0707a5ca4e514a5b456945b
Kagan Ozupek <kgn.ozupek26@gmail.com>
2015-12-17 13:13:40 +0200

    Initial Commit

---
+++ SocialDrinker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocialDrinker
+//
+//  Created by Kagan Ozupek on 17/12/15.
+//  Copyright © 2015 Kagan Ozupek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
